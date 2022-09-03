fd767f2f849050b9ee5da69c9d9536b327705246
caiozenke <caiolzenke@gmail.com>
2022-09-02 21:56:08 -0300

    ok

--- python_curso/func_lambda.py
+++ python_curso/func_lambda.py
@@ -11,20 +11,24 @@ print(var)
 
 v= lambda num1,num2 : num1* num2
 
-print(v(9,2))
+#print(v(9,2))
 
 lista =[
     ['P1', 10],
     ['P2', 2],
-    ['P1', 8],
-    ['P1', 5],
-    ['P1', 4],
-    ['P1', 12],
+    ['P3', 8],
+    ['P4', 5],
+    ['P5', 4],
+    ['P6', 12],
 ]
 
-def ordernar(produto):
-    return produto[1]
 
-lista.sort(key=ordernar ,reverse=True)
+"""def ordernar(produto):
+    return produto[1] """
+#lista.sort(key=lambda item: item[1] ,reverse=True) #ordenada com função lambda
 
-print(lista)
+print(sorted(lista, key=lambda produto: produto[0], reverse=True)) #ordena por maior ao menor
+'''
+for produto in lista:
+    print(f'produto {produto[0]} valor: {produto[1]}')
+    '''
