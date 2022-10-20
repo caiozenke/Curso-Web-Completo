184c7b367a56dfc4c4564b74885df2379de297ea
Caio Luan Zenke <74631392+caiozenke@users.noreply.github.com>
2022-10-19 21:19:30 -0300

    Constantes e Variaveis em js

---
+++ js/intro_js.js
@@ -0,0 +1,35 @@
+// comentario de uma linha
+/* comentario de mais linhas
+*/
+
+document.write('oLÁ mUNDO!')
+console.log("olá mundo com console")
+alert('Olá Mundo com Alert')
+
+
+// Variaveis e Constantes
+
+let texto = "uso do let"
+let texto2 = "outro  texto2"
+let meuPeso = 67.2
+let minhaAltura = 1.81
+let minhaIdade = 18
+let DoadorSangue = true
+let testenull = null
+
+
+// const (valor constante)
+
+const PI = 3.14
+const TAXA = 0.5
+
+// Escrever como fosse o print
+
+testenull = 'Não é null'
+
+
+document.write("<p> Texto 1: " + texto + "</p>")//Concatenar 
+document.write("<p> Texto 2: " + texto2 + " </p>"),
+document.write("<p> Minha Altura :" + minhaAltura + "</p>" + "<p> Minha Idade :" + minhaIdade + "</p>")
+document.write("<p> Constante de Pi :" + (PI) + "</p>")
+document.write(TAXA)
